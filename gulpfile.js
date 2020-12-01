c03697623134f0d81e14b9a8a7fed346d7922ea5
Mike <k2punk@gmail.com>
2020-12-01 11:52:29 +0300

    finish second step

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,44 @@ var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 var svgSprite = require('gulp-svg-sprite');
 var concat = require('gulp-concat');
+var gulpCopy = require('gulp-copy');
+var outputPath = './dist/';
  
+gulp.task('copy', function() {
+  return gulp.src('./app/images/*.jpg')
+    .pipe(gulpCopy(outputPath,{ prefix: 1 }))
+});
+
 gulp.task('scripts', function() {
   return gulp.src('./app/*.js')
     .pipe(concat('all.js'))
     .pipe(gulp.dest('./dist/'));
 });
  
-gulp.src('./app/*.svg')
-  .pipe(svgSprite(/* ... Insert your configuration here ... */))
-  .pipe(gulp.dest('./dist/'));
+gulp.task('sprite', function() {
+  return gulp.src('./app/**/*.svg')
+  .pipe(svgSprite({
+    mode: {
+      stack: {
+        sprite: "../sprite.svg"  //sprite file name
+      }
+    }
+  }))
+  .pipe(gulp.dest('./dist/images/icons'));
+});
 
 gulp.task('views', function buildHTML() {
   return gulp.src('./app/*.pug')
-  .pipe(pug({
-    // Your options in here.
-  }))
+  .pipe(pug({}))
+  .pipe(gulp.dest('./dist'));
 });
  
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./app/**/*.scss')
+  return gulp.src('./app/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./dist/'));
 });
  
 gulp.task('sass:watch', function () {
